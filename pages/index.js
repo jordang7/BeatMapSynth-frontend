59d728b24e76d600bee142194a7f023584365ffc
jordang7 <77jgreenberg77@gmail.com>
2023-03-15 15:35:47 -0400

    working form, need to add correct backend URL

---
+++ pages/index.js
@@ -0,0 +1,104 @@
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Button,
+  Input,
+  RadioGroup,
+  HStack,
+  Radio,
+  Center,
+  Box,
+} from "@chakra-ui/react";
+import { Field, Form, Formik } from "formik";
+
+const BASE_URL = "https://beatmapsynth.com/convert"; //CHANGE THIS TO CURRENT BACKEND URL
+
+export default function Home() {
+  function validateName(value) {
+    let error;
+    if (!value) {
+      error = "Song is required";
+    }
+    return error;
+  }
+
+  return (
+    <Center height="100vh" bgColor="gray.700" bgRepeat={true}>
+      <Formik
+        initialValues={{ song: "", difficulty: "normal" }}
+        onSubmit={async (values, { resetForm }) => {
+          const response = await fetch(
+            `${BASE_URL}/search?url=${values.song}&difficulty=${values.difficulty}`
+          );
+          console.log(response);
+          resetForm();
+        }}
+      >
+        {(props) => (
+          <Form>
+            <Box m={2} p={4} bgColor="white" borderRadius={"8px"}>
+              <Field name="song" validate={validateName}>
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={form.errors.song && form.touched.song}
+                  >
+                    <FormLabel>Input your song</FormLabel>
+                    <Input
+                      {...field}
+                      placeholder="https://www.youtube.com/..."
+                    />
+                    <FormErrorMessage>{form.errors.song}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+            </Box>
+            <Box m={2} p={4} bgColor="white" borderRadius={"8px"}>
+              <Field name="difficulty">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={
+                      form.errors.difficulty && form.touched.difficulty
+                    }
+                  >
+                    <FormLabel as="legend">Select Difficulty Level</FormLabel>
+                    <RadioGroup {...field}>
+                      <HStack spacing="24px">
+                        <Radio {...field} value="easy">
+                          Easy
+                        </Radio>
+                        <Radio {...field} value="normal">
+                          Normal
+                        </Radio>
+                        <Radio {...field} value="hard">
+                          Hard
+                        </Radio>
+                        <Radio {...field} value="expert">
+                          Expert
+                        </Radio>
+                        <Radio {...field} value="expertPlus">
+                          ExpertPlus
+                        </Radio>
+                      </HStack>
+                    </RadioGroup>
+                  </FormControl>
+                )}
+              </Field>
+            </Box>
+            <Center>
+              <Button
+                mt={4}
+                colorScheme="teal"
+                isLoading={props.isSubmitting}
+                type="submit"
+                width="30%"
+              >
+                Submit
+              </Button>
+            </Center>
+          </Form>
+        )}
+      </Formik>
+    </Center>
+  );
+}
